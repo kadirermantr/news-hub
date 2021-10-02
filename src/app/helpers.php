1e7046f5143acb31a756b5fc52e76b5facea40b9
=Kadir Erman <kadirermantr@gmail.com>
2021-10-02 11:24:26 +0300

    view fonksiyonu eklendi

--- src/app/helpers.php
+++ src/app/helpers.php
@@ -3,12 +3,10 @@
 function publicPath(?string $path = null)
 {
     $env = parse_ini_file(__DIR__ . '/../.env');
-    return $env['APP_URL'] . $path;
+    echo $env['APP_URL'] . '/' . $path;
 }
 
-function view($view, array $data = [])
+function view($view, string $title, array $data = []): mixed
 {
-    require_once __DIR__ . "/views/".$view.".php";
-
-    return $data;
+    require_once __DIR__ . '/views/'.$view.'.php';
 }

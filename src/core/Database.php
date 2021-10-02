69b834512f30d7a035dc2832a5abcf4d6e1d749a
=Kadir Erman <kadirermantr@gmail.com>
2021-10-02 08:07:04 +0300

    veritabanı bağlantısı yapıldı

--- src/core/Database.php
+++ src/core/Database.php
@@ -1,1 +1,28 @@
+<?php
 
+namespace Core;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    public static function connection()
+    {
+        $env = parse_ini_file(__DIR__ . '/../.env');
+
+        $host = $env['DB_HOST'];
+        $database = $env['DB_DATABASE'];
+        $username = $env['DB_USERNAME'];
+        $password = $env['DB_PASSWORD'];
+
+        try {
+            $dsn = "mysql:host=$host; dbname=$database;";
+            $pdo = new PDO($dsn, $username, $password);
+            //return $pdo;
+        } catch (PDOException $e) {
+            echo "Veritabanı hatası " . $e->getMessage();
+            exit(1);
+        }
+    }
+}

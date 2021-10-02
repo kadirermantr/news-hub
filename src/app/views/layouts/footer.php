e6f445d9a0ffb354add6ce5001c5a6d0a493623f
=Kadir Erman <kadirermantr@gmail.com>
2021-10-02 11:25:23 +0300

    view dosyaları oluşturuldu

---
+++ src/app/views/layouts/footer.php
@@ -0,0 +1,17 @@
+
+
+<footer class="bg-dark text-light fst-italic">
+    <div class="container py-3">
+        <div class="row mt-3">
+            <div class="col-md-12 text-center">
+                <p>
+                    Copyright &copy; 2021 ~ Tüm hakları saklıdır.
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="<?php publicPath('js/bootstrap.bundle.min.js')?>"></script>
+</body>
+</html>

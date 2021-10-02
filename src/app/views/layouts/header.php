f6fc8d20bdc90daaf86eaf357822f569a98cef42
=Kadir Erman <kadirermantr@gmail.com>
2021-10-02 11:30:41 +0300

    navbar parçalandı

--- src/app/views/layouts/header.php
+++ src/app/views/layouts/header.php
@@ -8,37 +8,5 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Navbar">
-    <div class="container">
-        <a class="navbar-brand" href="#">Haberler</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Anasayfa</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdown07" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                    <ul class="dropdown-menu" aria-labelledby="dropdown07">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-            </ul>
-
-            <div class="navbar-nav ml-auto">
-                <a href="/login" class="btn btn-primary me-2">Oturum Aç</a>
-            </div>
-        </div>
-    </div>
-</nav>
+<?php require 'navbar.php';?>
 

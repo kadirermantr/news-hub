6ef1262595605d3d62fbf6600ce95334e2a918f5
=Kadir Erman <kadirermantr@gmail.com>
2021-10-04 18:45:08 +0300

    footer dosyası düzenlendi

--- src/app/views/layouts/footer.php
+++ src/app/views/layouts/footer.php
@@ -14,6 +14,7 @@
 --->
 
 </main>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="<?php publicPath('js/bootstrap.bundle.min.js')?>"></script>
 </body>

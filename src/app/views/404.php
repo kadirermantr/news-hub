c7bdc0beecc958a01db575b1b4e0ce3c9c4c9612
=Kadir Erman <kadirermantr@gmail.com>
2021-10-05 14:33:12 +0300

    sayfa içeriği güncellendi

--- src/app/views/404.php
+++ src/app/views/404.php
@@ -1,3 +1,20 @@
-<?php
+<?php require __DIR__ . '/layouts/header.php';?>
 
-echo "welcome to 404 page | not found page :(";
+    <div class="container">
+        <div class="row justify-content-center py-5">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header text-uppercase text-center">
+                        <strong>Hata | 404</strong>
+                    </div>
+
+                    <div class="card-body">
+                        <p>Üzgünüz, aradığınız sayfa bulunamadı.</p>
+                        <a href="/">» Anasayfaya geri dön.</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php require __DIR__ . '/layouts/footer.php';?>

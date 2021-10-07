1b5849e6362e9d17f9d4ee90d081b85bc9fa1c3a
Kadir Erman <kadirermantr@gmail.com>
2021-10-07 10:29:45 +0300

    Gereksiz alanlar kaldırıldı

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -3,34 +3,8 @@
 namespace App\Models;
 
 use Core\Model;
-use PDO;
 
 class User extends Model
 {
-    protected static $table = 'users';
-    protected static $id_column = 'user_id';
-
-    public static function create(array $data) {
-        return parent::getCreate(self::$table, $data);
-    }
-
-    public static function all(): array
-    {
-        return parent::getAll(self::$table);
-    }
-
-    public static function find(mixed $id): mixed
-    {
-        return parent::getFind(self::$table, self::$id_column, $id);
-    }
-
-    public static function delete(mixed $id): bool
-    {
-        return parent::getDelete(self::$table, self::$id_column, $id);
-    }
-
-    public static function where(string $columnName, mixed $id): mixed
-    {
-        return parent::getWhere(self::$table, $columnName, $id);
-    }
+    protected static $table = "users";
 }

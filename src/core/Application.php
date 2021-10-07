761fb913658e29690b408fe66c0f287dda432692
Kadir Erman <kadirermantr@gmail.com>
2021-10-07 10:59:08 +0300

    controller ile ilgili fonksiyonlar eklendi

--- src/core/Application.php
+++ src/core/Application.php
@@ -6,6 +6,7 @@ class Application
 {
     public Router $router;
     public Request $request;
+    public Controller $controller;
     public static Application $app;
 
     public function __construct()
@@ -20,5 +21,15 @@ class Application
     public function run()
     {
         echo $this->router->resolve();
+    }
+
+    public function getController(): Controller
+    {
+        return $this->controller;
+    }
+
+    public function setController(Controller $controller): void
+    {
+        $this->controller = $controller;
     }
 }

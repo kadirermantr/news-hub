77085158dc2b0482363caa9cd5111afbcb9c66da
Kadir Erman <kadirermantr@gmail.com>
2021-10-08 03:18:07 +0300

    account için route yapısı eklendi

--- src/app/routes.php
+++ src/app/routes.php
@@ -2,6 +2,7 @@
 
 use App\Controllers\Auth\LoginController;
 use App\Controllers\Auth\RegisterController;
+use App\Controllers\Auth\UserController;
 use App\Controllers\HomeController;
 
 
@@ -11,4 +12,4 @@ $app->router->post('/register', [RegisterController::class, 'store']);
 $app->router->get('/login', [LoginController::class, 'index']);
 $app->router->post('/login', [LoginController::class, 'login']);
 $app->router->get('/logout', [LoginController::class, 'logout']);
-$app->router->get('/sil', [LoginController::class, 'delete']);
+$app->router->get('/account', [UserController::class, 'index']);

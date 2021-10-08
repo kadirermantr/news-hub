70ecaee67185cc07f3249f0f0f8f0a351a1db002
Kadir Erman <kadirermantr@gmail.com>
2021-10-08 10:16:12 +0300

    gereksiz kodlar kaldırıldı

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -8,9 +8,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $name = "Kadir";
-        $lastname = "Erman";
-
-        view('home', 'Homepage', compact('name', 'lastname'));
+        return $this->view('home', 'Anasayfa');
     }
 }

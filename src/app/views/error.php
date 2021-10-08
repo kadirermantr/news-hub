80e8ad1f19a9142586112a528ce3e9cb1551c768
Kadir Erman <kadirermantr@gmail.com>
2021-10-08 03:17:11 +0300

    hatalar için sayfa oluşturuldu

---
+++ src/app/views/error.php
@@ -0,0 +1,20 @@
+<?php require __DIR__ . '/layouts/header.php';?>
+
+    <div class="container text-center">
+        <div class="row justify-content-center py-5">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header text-uppercase text-center">
+                        <strong>HTTP ERROR | <?= $code ?></strong>
+                    </div>
+
+                    <div class="card-body">
+                        <p><?= $description ?></p>
+                        <a href="/">» Anasayfaya geri dön.</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php require __DIR__ . '/layouts/footer.php';?>

290fbe04f6d6d3f85ac925e22304a1fd2462c575
Kadir Erman <kadirermantr@gmail.com>
2021-10-09 23:05:10 +0300

    yeni rotalar eklendi

--- src/app/routes.php
+++ src/app/routes.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Controllers\Admin\CategoryController;
+use App\Controllers\Admin\UserController as aUserController;
+use App\Controllers\Admin\NewsController;
 use App\Controllers\Auth\LoginController;
 use App\Controllers\Auth\RegisterController;
 use App\Controllers\Auth\UserController;
 use App\Controllers\HomeController;
+use Core\Controller;
 
 
 $app->router->get('/', [HomeController::class, 'index']);
@@ -15,3 +19,20 @@ $app->router->post('/login', [LoginController::class, 'login']);
 
 $app->router->get('/logout', [UserController::class, 'logout']);
 $app->router->get('/account', [UserController::class, 'index']);
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+$app->router->get('/admin', function () {
+    return Controller::view('auth/admin/index', 'Yönetim Paneli');
+});
+
+
+$app->router->get('/admin/category', [CategoryController::class, 'index']);
+$app->router->get('/admin/category/edit', [CategoryController::class, 'edit']);
+$app->router->post('/admin/category/edit', [CategoryController::class, 'update']);
+
+
+
+$app->router->get('/admin/news', [NewsController::class, 'index']);
+$app->router->get('/admin/users', [aUserController::class, 'index']);

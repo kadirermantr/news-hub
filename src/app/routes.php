c2d6e1651b1460be839effeaf3851111e64c9592
Kadir Erman <kadirermantr@gmail.com>
2021-10-11 04:24:03 +0300

    yeni rotalar eklendi

--- src/app/routes.php
+++ src/app/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controllers\Admin\AdminController;
 use App\Controllers\Admin\CategoryController;
 use App\Controllers\Admin\UserController as aUserController;
 use App\Controllers\Admin\NewsController;
@@ -23,16 +24,24 @@ $app->router->get('/account', [UserController::class, 'index']);
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
-$app->router->get('/admin', function () {
-    return Controller::view('auth/admin/index', 'Yönetim Paneli');
-});
-
+$app->router->get('/admin', [AdminController::class, 'index']);
 
 $app->router->get('/admin/category', [CategoryController::class, 'index']);
+$app->router->get('/admin/category/create', [CategoryController::class, 'create']);
+$app->router->post('/admin/category/create', [CategoryController::class, 'store']);
 $app->router->get('/admin/category/edit', [CategoryController::class, 'edit']);
 $app->router->post('/admin/category/edit', [CategoryController::class, 'update']);
 
 
+$app->router->get('/admin/user', [aUserController::class, 'index']);
+$app->router->get('/admin/user/create', [aUserController::class, 'create']);
+$app->router->post('/admin/user/create', [aUserController::class, 'store']);
+$app->router->get('/admin/user/edit', [aUserController::class, 'edit']);
+$app->router->post('/admin/user/edit', [aUserController::class, 'update']);
+
 
 $app->router->get('/admin/news', [NewsController::class, 'index']);
-$app->router->get('/admin/users', [aUserController::class, 'index']);
+$app->router->get('/admin/news/create', [NewsController::class, 'create']);
+$app->router->post('/admin/news/create', [NewsController::class, 'store']);
+$app->router->get('/admin/news/edit', [NewsController::class, 'edit']);
+$app->router->post('/admin/news/edit', [NewsController::class, 'update']);

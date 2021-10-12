6cf589a109c2654008227c5e94dff59897721cc9
Kadir Erman <kadirermantr@gmail.com>
2021-10-12 16:04:15 +0300

    comment modeli oluşturuldu

---
+++ src/app/Models/Comment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Core\Model;
+
+class Comment extends Model
+{
+    protected static $table = "comments";
+
+    public function getUser(int $user_id)
+    {
+        return User::find($user_id);
+    }
+
+    public function getNews(int $news_id)
+    {
+        return News::find($news_id);
+    }
+
+    public function getSummary(string $data)
+    {
+        $end = strlen($data) > 30 ? "..." : "";
+        return mb_substr($data, 0, 30) . $end;
+    }
+}

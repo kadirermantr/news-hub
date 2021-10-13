4284a5c8ffefd5ff2a0b92d5c9d112462040341a
Kadir Erman <kadirermantr@gmail.com>
2021-10-13 13:05:44 +0300

    takip edilen kategorilerdeki haberler gösterildi

--- src/app/Controllers/Admin/DashboardController.php
+++ src/app/Controllers/Admin/DashboardController.php
@@ -4,6 +4,8 @@ namespace App\Controllers\Admin;
 
 use App\Middlewares\Authenticate;
 use App\Models\Comment;
+use App\Models\FollowedCategories;
+use App\Models\News;
 use App\Models\User;
 use Core\Controller;
 
@@ -16,14 +18,35 @@ class DashboardController extends Controller
 
     public function index()
     {
+        $news = array_reverse(News::all());
         $comments = array_reverse(Comment::all());
+        $users = FollowedCategories::where('user_id', user('id'));
+
+        for ($i=0; $i < count($news); $i++) {
+            $news[$i]['is_following'] = false;
+        }
+
+        for ($i=0; $i < count($news); $i++) {
+            foreach ($users as $user_following) {
+                if ($news[$i]['category_id'] == $user_following['category_id']) {
+                    $news[$i]['is_following'] = true;
+
+                    $user = (new News())->getUser($news[$i]['user_id']);
+                    $category = (new News())->getCategory($news[$i]['category_id']);
+
+                    $news[$i]['user'] = $user['name'] . " " . $user['lastname'];
+                    $news[$i]['category'] = $category['name'];
+                    $news[$i]['date'] = date("d/m/Y", strtotime($news[$i]['date']));
+                }
+            }
+        }
 
         for ($i=0; $i < count($comments); $i++) {
-            $news = (new User())->getNews($comments[$i]['news_id']);
-            $comments[$i]['news'] = $news['title'];
+            $comment_news = (new User())->getNews($comments[$i]['news_id']);
+            $comments[$i]['news'] = $comment_news['title'];
             $comments[$i]['date'] = date("d/m/Y - H:i", strtotime($comments[$i]['date']));
         }
 
-        return $this->view('auth/admin/dashboard', 'Kontrol Paneli', compact('comments'));
+        return $this->view('auth/admin/dashboard', 'Kontrol Paneli', compact('news', 'comments'));
     }
 }
--- src/app/views/auth/admin/dashboard.php
+++ src/app/views/auth/admin/dashboard.php
@@ -9,10 +9,61 @@
                         <li class="breadcrumb-item active" aria-current="page"><?= $title ?></li>
                     </ol>
                 </nav>
+            </div>
+        </div>
+    </div>
 
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-body">
-                        <h5>Yorumlarım:</h5>
+                        <h5 class="py-2 text-center text-danger">Takip ettiğim kategorilerde son haberler</h5>
+
+                        <table class="table caption-top table-hover">
+                            <thead>
+                            <tr>
+                                <th scope="col">Başlık</th>
+                                <th scope="col">Kategori</th>
+                                <th scope="col">Tarih</th>
+                                <th scope="col">Yazar</th>
+                                <th scope="col"></th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            <?php foreach($news as $post): ?>
+                                <?php if ($post['is_following']): ?>
+                                <tr>
+                                    <td><a href="news?id=<?= $post['id']?>" target="_blank"><?= $post['title'] ?></a></td>
+                                    <td><?= $post['category'] ?></td>
+                                    <td><?= $post['date'] ?></td>
+                                    <td><?= $post['user'] ?>
+                                    </td>
+                                    <td>
+                                        <a href="news/edit?id=<?= $post['id'] ?>">
+                                            <span class="btn btn-outline-primary">
+                                            <i class="fa fa-edit"></i>
+                                            </span>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="py-2 text-center text-danger">Yorumlarım</h5>
 
                         <table class="table caption-top table-hover">
                             <thead>
@@ -26,12 +77,12 @@
                             <tbody>
                             <?php foreach($comments as $comment): ?>
                                 <?php if ($comment['user_id'] === user('id')): ?>
-                                <tr>
-                                    <td><?= $comment['content'] ?></td>
-                                    <td><a href="/news?id=<?= $comment['news_id'] ?>" target="_blank"><?= $comment['news'] ?></a></td>
-                                    <td><?= $comment['date'] ?></td>
-                                    </td>
-                                </tr>
+                                    <tr>
+                                        <td><?= $comment['content'] ?></td>
+                                        <td><a href="/news?id=<?= $comment['news_id'] ?>" target="_blank"><?= $comment['news'] ?></a></td>
+                                        <td><?= $comment['date'] ?></td>
+                                        </td>
+                                    </tr>
                                 <?php endif; ?>
                             <?php endforeach; ?>
                             </tbody>
